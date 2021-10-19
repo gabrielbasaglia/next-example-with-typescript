78d1a0dc2964eff1a266c162de01fe61b79affa3
Gabriel Basaglia <90632084+gabrielbasaglia@users.noreply.github.com>
2021-10-19 17:20:38 -0300

    Atualização

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1>MEU HEADER</h1>
+    </header>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-
   render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
           <link rel="shortcut icon" href="/favicon.png" type="image.png" />
         </Head>
         <body>
@@ -15,8 +17,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
